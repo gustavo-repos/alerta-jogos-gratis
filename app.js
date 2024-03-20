fc8c1a40d76b41d7497b28b1714e21cab38329e2
Gustavo <gbrocchetto@gmail.com>
2024-03-20 12:33:46 -0300

    try catch no puppeteer

--- app.js
+++ app.js
@@ -148,35 +148,39 @@ process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
 const puppeteer = require('puppeteer-core')
 
 async function extractHrefValues(url) {
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
-        headless: true
-    })
-    const page = await browser.newPage()
+    try {
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser',
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+            headless: true
+        })
+        const page = await browser.newPage()
 
-    await page.setExtraHTTPHeaders({ 
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-		'upgrade-insecure-requests': '1', 
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
-		'accept-encoding': 'gzip, deflate, br', 
-		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
-	}); 
-  
-    await page.goto(url);
+        await page.setExtraHTTPHeaders({ 
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+            'upgrade-insecure-requests': '1', 
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+            'accept-encoding': 'gzip, deflate, br', 
+            'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+        }); 
+    
+        await page.goto(url);
 
-    const hrefValues = await page.evaluate(() => {
-        var aTags = document.getElementsByTagName('a')
-        var links = []
-        for (var i = 0; i < aTags.length; i++) {
-            links.push(aTags[i].href)
-        }
-        return links
-    })
-  
-    await browser.close()
-  
-    return hrefValues
+        const hrefValues = await page.evaluate(() => {
+            var aTags = document.getElementsByTagName('a')
+            var links = []
+            for (var i = 0; i < aTags.length; i++) {
+                links.push(aTags[i].href)
+            }
+            return links
+        })
+    
+        await browser.close()
+    
+        return hrefValues
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 extractHrefValues('https://store.epicgames.com/pt-BR/free-games')
