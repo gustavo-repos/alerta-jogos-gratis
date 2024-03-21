7f703a99c1dfb03aeed445f330567206039cace4
Gustavo <gbrocchetto@gmail.com>
2024-03-20 21:52:31 -0300

    plugin extra aws

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ puppeteer.use(awsPlugin());
 
 async function extractHrefValues(url) {
     const browser = await puppeteer.launch({
-        executablePath: '/snap/bin/chromium',
+        executablePath: '/usr/bin/chromium-browser',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
