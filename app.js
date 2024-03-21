7ec10089c51fb88ca15cca662d1e3556dae1fd5b
Gustavo <gbrocchetto@gmail.com>
2024-03-21 18:09:54 -0300

    sem puppeteer para testar aws

--- app.js
+++ app.js
@@ -142,60 +142,60 @@ timing();
 
 
 
-// SCRAP
+// // SCRAP
 
-process.env.CHROME_BIN = '/usr/bin/chromium-browser';
-process.env.CHROME_PATH = '/usr/bin/chromium-browser';
-process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
+// process.env.CHROME_BIN = '/usr/bin/chromium-browser';
+// process.env.CHROME_PATH = '/usr/bin/chromium-browser';
+// process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
 
-// puppeteer-extra is a drop-in replacement for puppeteer,
-// it augments the installed puppeteer with plugin functionality
-const puppeteer = require('puppeteer-extra');
-// require AWS plugin
-const awsPlugin = require('puppeteer-extra-plugin-aws');
-// add AWS plugin
-puppeteer.use(awsPlugin());
+// // puppeteer-extra is a drop-in replacement for puppeteer,
+// // it augments the installed puppeteer with plugin functionality
+// const puppeteer = require('puppeteer-extra');
+// // require AWS plugin
+// const awsPlugin = require('puppeteer-extra-plugin-aws');
+// // add AWS plugin
+// puppeteer.use(awsPlugin());
 
 
-//const puppeteer = require('puppeteer-core')
+// //const puppeteer = require('puppeteer-core')
 
-async function extractHrefValues(url) {
-    const browser = await puppeteer.launch({
-        executablePath: '/snap/bin/chromium',
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-web-security'
-          ]
-    })
-    const page = await browser.newPage()
+// async function extractHrefValues(url) {
+//     const browser = await puppeteer.launch({
+//         executablePath: '/snap/bin/chromium',
+//         args: [
+//             '--no-sandbox',
+//             '--disable-setuid-sandbox',
+//             '--disable-dev-shm-usage',
+//             '--disable-web-security'
+//           ]
+//     })
+//     const page = await browser.newPage()
 
-    await page.setExtraHTTPHeaders({ 
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-		'upgrade-insecure-requests': '1', 
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
-		'accept-encoding': 'gzip, deflate, br', 
-		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
-	}); 
+//     await page.setExtraHTTPHeaders({ 
+// 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+// 		'upgrade-insecure-requests': '1', 
+// 		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+// 		'accept-encoding': 'gzip, deflate, br', 
+// 		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+// 	}); 
   
-    await page.goto(url);
+//     await page.goto(url);
 
-    const hrefValues = await page.evaluate(() => {
-        var aTags = document.getElementsByTagName('a')
-        var links = []
-        for (var i = 0; i < aTags.length; i++) {
-            links.push(aTags[i].href)
-        }
-        return links
-    })
+//     const hrefValues = await page.evaluate(() => {
+//         var aTags = document.getElementsByTagName('a')
+//         var links = []
+//         for (var i = 0; i < aTags.length; i++) {
+//             links.push(aTags[i].href)
+//         }
+//         return links
+//     })
   
-    await browser.close()
+//     await browser.close()
   
-    return hrefValues
-}
+//     return hrefValues
+// }
 
-extractHrefValues('https://www.gog.com/partner/free_games')
-    .then(result => {
-        console.log(result)
-    })
+// extractHrefValues('https://www.gog.com/partner/free_games')
+//     .then(result => {
+//         console.log(result)
+//     })
