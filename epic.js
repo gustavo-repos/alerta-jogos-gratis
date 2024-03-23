a1f836ab6bc320301d480841d0f0d18e9f8bbf45
Gustavo <gbrocchetto@gmail.com>
2024-03-22 23:11:12 -0300

    for 180

--- epic.js
+++ epic.js
@@ -75,8 +75,8 @@ const getFreeGames = async () => {
         return data
     }
     
-    for (var i = 0; i < links.length; i++) {
-    //for (var i = 0; i < 10; i++) {
+    //for (var i = 0; i < links.length; i++) {
+    for (var i = 0; i < 180; i++) {
         var scrapedData = await scrapData(links[i])        
 
         var freeGame = {
