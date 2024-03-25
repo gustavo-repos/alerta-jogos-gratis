8bf86f8a5e3d8cd0196ef3e6f8259dfaec3bc85e
Gustavo <gbrocchetto@gmail.com>
2024-03-25 19:00:30 -0300

    teste

--- epic.js
+++ epic.js
@@ -110,7 +110,7 @@ const getFreeGames = async () => {
     //for (var i = 0; i < links.length; i++) {
     //for (var i = 0; i < 180; i++) { 
     //var browserOpen = await puppeteer.launch(launchOptions)
-    for (var i = 20; i < 40; i++) {
+    for (var i = 20; i < 60; i++) {
         const startTime = new Date().getTime();
         var scrapedData = await scrapData(links[i])        
 
