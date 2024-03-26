3cd1ffdabd7e7e7d423324ace3a66357b50a4b4a
Gustavo <gbrocchetto@gmail.com>
2024-03-26 16:17:24 -0300

    teste

--- epic.js
+++ epic.js
@@ -1,171 +1,204 @@
-const FreeGame = require('./models/freeGame');
+const FreeGame = require('./models/freeGame')
 const UpdateLog = require('./models/updateLog');
-var date;
-var startTime;
-var endTime;
-var log = [];
+var date
+var startTime
+var endTime
+var log = []
 
-const { sendNotification } = require('./notifications');
+const { sendNotification } = require('./notifications')
 
 process.env.CHROME_BIN = '/usr/bin/chromium-browser';
 process.env.CHROME_PATH = '/usr/bin/chromium-browser';
 process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
 
-const { launchOptions, headerOptions } = require('./puppeteerOptions');
-const puppeteer = require('puppeteer-core');
+const { launchOptions, headerOptions } = require('./puppeteerOptions')
+const puppeteer = require('puppeteer-core')
 
 const getFreeGames = async () => {
-    date = new Date();
-    startTime = date.getTime();
-    log.push(`Início da atualização da Epic ${date.toLocaleDateString("en-GB")} às ${parseInt(date.getHours()) - 3}:${String(date.getMinutes()).padStart(2, "0")}`);
+
+    date = new Date()
+    startTime = date.getTime()
+
+    log.push(`Início da atualização da Epic ${date.toLocaleDateString("en-GB")} às ${parseInt(date.getHours()) - 3}:${String(date.getMinutes()).padStart(2, "0")}`)
 
     var freeGames = [];
 
     async function extractHrefValues(url) {
-        const browser = await puppeteer.launch(launchOptions);
+        const browser = await puppeteer.launch(launchOptions)
 
         try {
-            const page = await browser.newPage();
-            await page.setCacheEnabled(false);
-            await page.setExtraHTTPHeaders(headerOptions);
-            await page.goto(url, { waitUntil: 'load', timeout: 0 });
-
-            await page.waitForSelector('.css-g3jcms');
-
-            const hrefValues = await page.evaluate(() => {
-                const elements = document.querySelectorAll('.css-g3jcms');
-                const hrefs = [];
-                for (let i = 0; i < elements.length; i++) {
-                    hrefs.push(elements[i].href);
-                }
-                return hrefs;
-            });
-            return hrefValues;
+          const page = await browser.newPage()
+          await page.setCacheEnabled(false)
+          await page.setExtraHTTPHeaders(headerOptions); 
+          await page.goto(url, { waitUntil: 'load', timeout: 0 });
+  
+          await page.waitForSelector('.css-g3jcms');
+  
+          const hrefValues = await page.evaluate(() => {
+              const elements = document.querySelectorAll('.css-g3jcms');
+              const hrefs = [];
+              for (let i = 0; i < elements.length; i++) {
+                hrefs.push(elements[i].href);
+              }
+              return hrefs;
+          })
+          return hrefValues
         } catch (error) {
             console.log(error);
         } finally {
-            await browser.close();
+            await browser.close()
         }
+        
     }
 
-    var links = await extractHrefValues('https://store.epicgames.com/pt-BR/browse?sortBy=releaseDate&sortDir=DESC&priceTier=tierFree&category=Game&count=300&start=0');
+    var links = await extractHrefValues('https://store.epicgames.com/pt-BR/browse?sortBy=releaseDate&sortDir=DESC&priceTier=tierFree&category=Game&count=300&start=0')
 
     async function scrapData(url) {
-        const browser = await puppeteer.launch(launchOptions);
+        const browser = await puppeteer.launch(launchOptions)
 
         try {
-            const page = await browser.newPage();
-            await page.setCacheEnabled(false);
-            await page.setExtraHTTPHeaders(headerOptions);
-            await page.goto(url, { waitUntil: 'load', timeout: 0 });
+          const page = await browser.newPage()
+          await page.setCacheEnabled(false)
+          await page.setExtraHTTPHeaders(headerOptions); 
+          await page.goto(url, { waitUntil: 'load', timeout: 0 });
+  
+          await page.waitForSelector('.css-1mzagbj', {timeout: 0})
+          await page.waitForSelector('.css-vs1xw0', {timeout: 0})
+          try {
+              await page.waitForSelector('.css-1bbjmcj', {timeout: 0}) // acho que isso nao pode ser infinito
+          } catch (error) {
+              await page.waitForSelector('.css-7i770w', {timeout: 0})
+          } 
+  
+          const data = await page.evaluate(() => {
 
-            await page.waitForSelector('.css-1mzagbj', { timeout: 0 });
-            await page.waitForSelector('.css-vs1xw0', { timeout: 0 });
-            try {
-                await page.waitForSelector('.css-1bbjmcj', { timeout: 0 });
-            } catch (error) {
-                await page.waitForSelector('.css-7i770w', { timeout: 0 });
-            }
-
-            const data = await page.evaluate(() => {
-                const title = document.querySelector('.css-1mzagbj').textContent;
-                var elements;
-                elements = document.querySelector('.css-vs1xw0').childNodes;
-                const genres = [];
-                for (let j = 0; j < elements.length; j++) {
-                    genres.push(elements[j].textContent);
-                }
-                if (document.querySelectorAll('.css-1bbjmcj').length > 0) {
-                    elements = document.querySelectorAll('.css-1bbjmcj');
-                } else {
-                    elements = document.querySelectorAll('.css-7i770w');
-                }
-                const srcs = [];
-                for (let j = 0; j < elements.length; j++) {
-                    srcs.push(elements[j].src);
-                }
-                return [title, genres.join(' - '), srcs];
-            });
-            return data;
+              const title = document.querySelector('.css-1mzagbj').textContent
+  
+              var elements
+  
+              elements = document.querySelector('.css-vs1xw0').childNodes
+              const genres = []
+              for (let j = 0; j < elements.length; j++) {
+                  genres.push(elements[j].textContent);
+              }
+  
+              if (document.querySelectorAll('.css-1bbjmcj').length > 0) {
+                elements = document.querySelectorAll('.css-1bbjmcj')
+              } else {
+                elements = document.querySelectorAll('.css-7i770w')
+              }
+              const srcs = []
+              for (let j = 0; j < elements.length; j++) {
+                  srcs.push(elements[j].src);
+              }
+  
+              return [title, genres.join(' - '), srcs]
+  
+          })
+          return data
         } catch (error) {
-            console.log(error);
+          console.log(error);
         } finally {
-            await browser.close();
+          await browser.close()
         }
+        
     }
-
+    
+    //var browser = await puppeteer.launch(launchOptions)
+    //console.log('Tamanho de link: '+links.length)
+    //for (var i = 0; i < links.length; i++) {
+    //for (var i = 0; i < 180; i++) { 
+    //var browserOpen = await puppeteer.launch(launchOptions)
     for (var i = 0; i < 20; i++) {
-        var scrapedData = await scrapData(links[i]);
+        var scrapedData = await scrapData(links[i])        
 
         var freeGame = {
-            title: scrapedData[0],
-            site: "Epic",
+            title: scrapedData[0], 
+            site: "Epic", 
             link: links[i],
             genre: scrapedData[1],
             images: scrapedData[2],
-        };
-        freeGames.push(freeGame);
+          }
+          freeGames.push(freeGame)
     }
-
-    date = new Date();
-    endTime = date.getTime();
-    log.push(`O scrap durou ${(endTime - startTime) / 1000}s.`);
+    date = new Date()
+    endTime = date.getTime()
+    log.push(`O scrap durou ${(endTime - startTime)/1000}s.`)
     return freeGames;
-};
+}
 
 async function sendEpicGames() {
-    var freeGames = await getFreeGames();
-    var newGames = [];
+
+    // var test = await getFreeGames()
+    // console.log(test) 
+
+    //fs.appendFileSync('./log.txt', `ATUALIZAÇÃO ${date.toLocaleDateString()} ${parseInt(date.getHours()) - 3}:${date.getMinutes()}\n`)
+
+    var freeGames = await getFreeGames()
+
+    var newGames = []
 
     for (var i = 0; i < freeGames.length; i++) {
+
         await (new FreeGame(freeGames[i])).save()
-            .then(() => {
-                newGames.push(freeGames[i]);
-            })
-            .catch(err => {
-                if (!err.code === 11000) {
-                    console.log(err);
-                }
-            });
+          .then(() => {
+            newGames.push(freeGames[i])
+          })
+          .catch(err => {
+            if (!err.code === 11000) {
+              console.log(err);
+            }
+            
+           });
+        
     }
 
     if (newGames.length > 0) {
         for (var i = 0; i < newGames.length; i++) {
-            log.push(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`);
-            sendNotification(`O jogo ${newGames[i].title} foi adicionado na plataforma ${newGames[i].site}!`);
-            console.log(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`);
+          //fs.appendFileSync('./log.txt', `Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).\n`)
+          log.push(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`)
+          sendNotification(`O jogo ${newGames[i].title} foi adicionado na plataforma ${newGames[i].site}!`)
+          console.log(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`)
         }
-    } else {
-        log.push(`Nenhum jogo novo adicionado na Epic.`);
-    }
+      } else {
+        //fs.appendFileSync('./log.txt', `Nenhum jogo novo adicionado na Epic.\n`)
+        log.push(`Nenhum jogo novo adicionado na Epic.`)
+        //console.log("Nenhum jogo novo adicionado na Epic.")
+      }
 
-    await FreeGame.find({ "site": "Epic" })
-        .then(async result => {
-            for (var i = 0; i < result.length; i++) {
-                var gameWasRemoved = true;
-                for (var j = 0; j < freeGames.length; j++) {
-                    if ((result[i].title === freeGames[j].title) && (result[i].site === freeGames[j].site)) {
-                        gameWasRemoved = false;
-                    }
-                }
-                if (gameWasRemoved) {
-                    await FreeGame.findOneAndDelete({ "title": result[i].title, "site": result[i].site })
-                        .then(result => {
-                            log.push(`Removido o jogo ${result.title} (${result.site}).`);
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                }
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
 
-    log.push(`Fim da atualização da Epic.`);
-    console.log(log);
-    await new UpdateLog({ log }).save();
+      await FreeGame.find({ "site": "Epic"}) 
+      .then(async result => {
+        for (var i = 0; i < result.length; i++) {
+          var gameWasRemoved = true;
+          for (var j = 0; j < freeGames.length; j++) {
+            if ((result[i].title === freeGames[j].title) && (result[i].site === freeGames[j].site)) {
+              gameWasRemoved = false;
+            }    
+          }
+          if (gameWasRemoved) {
+            await FreeGame.findOneAndDelete({ "title": result[i].title, "site": result[i].site })
+              .then(result => {
+                //fs.appendFileSync('./log.txt', `Removido o jogo ${result.title} (${result.site}).\n`)
+                log.push(`Removido o jogo ${result.title} (${result.site}).`)
+                //console.log(`Removido o jogo ${result.title} (${result.site}).`)
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  
+    log.push(`Fim da atualização da Epic.`)
+    //fs.appendFileSync('./log.txt', `Fim da atualização\n\n`)
+
+    console.log(log)
+    await new UpdateLog({log}).save()
+
 }
 
 module.exports = { sendEpicGames };
--- gog.js
+++ gog.js
@@ -1,108 +1,145 @@
 const FreeGame = require('./models/freeGame');
 const UpdateLog = require('./models/updateLog');
-const { makeRequest } = require('./scraper');
-var date;
-var startTime;
-var endTime;
-var log = [];
+const { makeRequest } = require('./scraper')
+var date
+var startTime
+var endTime
+var log = []
 
-const { sendNotification } = require('./notifications');
+const { sendNotification } = require('./notifications')
+
+
+// var newGames = [];
 
 const getFreeGames = async () => {
-    date = new Date();
-    startTime = date.getTime();
-    log.push(`Início da atualização da Gog ${date.toLocaleDateString("en-GB")} às ${parseInt(date.getHours()) - 3}:${String(date.getMinutes()).padStart(2, "0")}`);
+
+    date = new Date()
+    startTime = date.getTime()
+
+    log.push(`Início da atualização da Gog ${date.toLocaleDateString("en-GB")} às ${parseInt(date.getHours()) - 3}:${String(date.getMinutes()).padStart(2, "0")}`)
 
     var freeGames = [];
 
-    const mainPage = await makeRequest('https://www.gog.com/partner/free_games');
+    const mainPage = await makeRequest('https://www.gog.com/partner/free_games')
 
-    let elements = mainPage.getElementsByClassName('product-row__link');
+    let elements = mainPage.getElementsByClassName('product-row__link')
 
     let links = [];
     for (var element of elements) {
-        links.push('https://www.gog.com' + element.href);
+        links.push('https://www.gog.com'+element.href);
     }
 
     for (var i = 0; i < links.length; i++) {
-        const gamePage = await makeRequest(links[i]);
+        const gamePage =  await makeRequest(links[i])
 
-        function getImages() {
-            var imagesclassArray = gamePage.getElementsByClassName('productcard-thumbnails-slider__image');
-            var imagesSrc = [];
+          function getImages() {
+            var imagesclassArray = gamePage.getElementsByClassName('productcard-thumbnails-slider__image')
+            var imagesSrc = []
             for (var j = 0; j < imagesclassArray.length; j++) {
-                imagesSrc.push(imagesclassArray[j].src);
+              imagesSrc.push(imagesclassArray[j].src)
             }
-            return imagesSrc;
-        }
+            return imagesSrc
+          }
 
-        var freeGame = {
-            title: gamePage.querySelector('h1').textContent.trim(),
-            site: "Gog",
+          var freeGame = {
+            title: gamePage.querySelector('h1').textContent.trim(), 
+            site: "Gog", 
             link: links[i],
             genre: gamePage.getElementsByClassName('details__content table__row-content')[0].textContent.replace(/\s+/g, ' '),
+            // .replace(/\s/g, '').replace(/-/g, ' - ')
+            //system: gamePage.getElementsByClassName('table__row-content')[2].textContent.trim(),
             images: getImages(),
-        };
-        freeGames.push(freeGame);
+          }
+          //console.log(gamePage.getElementsByClassName('details__content table__row-content')[0].textContent.replace(/\s+/g, ' '))
+          freeGames.push(freeGame)
     }
-    date = new Date();
-    endTime = date.getTime();
-    log.push(`O scrap durou ${(endTime - startTime) / 1000}s.`);
+    date = new Date()
+    endTime = date.getTime()
+    log.push(`O scrap durou ${(endTime - startTime)/1000}s.`)
     return freeGames;
-};
+
+}
 
 async function sendGogGames() {
-    var freeGames = await getFreeGames();
-    var newGames = [];
 
-    for (var i = 0; i < freeGames.length; i++) {
-        await (new FreeGame(freeGames[i])).save()
-            .then(() => {
-                newGames.push(freeGames[i]);
-            })
-            .catch(err => {
-                if (!err.code === 11000) {
-                    console.log(err);
-                }
-            });
-    }
+  //var date = new Date();  
 
-    if (newGames.length > 0) {
-        for (var i = 0; i < newGames.length; i++) {
-            log.push(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`);
-            sendNotification(`O jogo ${newGames[i].title} foi adicionado na plataforma ${newGames[i].site}!`);
-        }
-    } else {
-        log.push(`Nenhum jogo novo adicionado na Gog.`);
-    }
+  //fs.appendFileSync('./log.txt', `ATUALIZAÇÃO ${date.toLocaleDateString()} ${parseInt(date.getHours()) - 3}:${date.getMinutes()}\n`)
 
-    await FreeGame.find({ "site": "Gog" })
-        .then(async result => {
-            for (var i = 0; i < result.length; i++) {
-                var gameWasRemoved = true;
-                for (var j = 0; j < freeGames.length; j++) {
-                    if ((result[i].title === freeGames[j].title) && (result[i].site === freeGames[j].site)) {
-                        gameWasRemoved = false;
-                    }
-                }
-                if (gameWasRemoved) {
-                    await FreeGame.findOneAndDelete({ "title": result[i].title, "site": result[i].site })
-                        .then(result => {
-                            log.push(`Removido o jogo ${result.title} (${result.site}).`);
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                }
-            }
+  var freeGames = await getFreeGames();
+
+  //início dummy content
+    // var freeGames = gameData;
+
+    // if (counter == 1) {
+    //   freeGames[0].title = 'Zeldinha';
+    // }
+
+    // counter++
+    //fim dump content
+
+  var newGames = []
+
+  for (var i = 0; i < freeGames.length; i++) {
+
+      await (new FreeGame(freeGames[i])).save()
+        .then(() => {
+          newGames.push(freeGames[i])
         })
         .catch(err => {
+          if (!err.code === 11000) {
             console.log(err);
-        });
+          }
+          
+         });
+      
+  }
 
-    log.push(`Fim da atualização da Gog.`);
-    console.log(log);
-    await new UpdateLog({ log }).save();
+  if (newGames.length > 0) {
+    for (var i = 0; i < newGames.length; i++) {
+      //fs.appendFileSync('./log.txt', `Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).\n`)
+      log.push(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`)
+      sendNotification(`O jogo ${newGames[i].title} foi adicionado na plataforma ${newGames[i].site}!`)
+      //console.log(`Adicionado o jogo ${newGames[i].title} (${newGames[i].site}).`)
+    }
+  } else {
+    log.push(`Nenhum jogo novo adicionado na Gog.`)
+    //fs.appendFileSync('./log.txt', `Nenhum jogo novo adicionado na Gog.\n`)
+    //console.log("Nenhum jogo novo adicionado na Gog.")
+  }
+
+
+  await FreeGame.find({ "site": "Gog"}) 
+    .then(async result => {
+      for (var i = 0; i < result.length; i++) {
+        var gameWasRemoved = true;
+        for (var j = 0; j < freeGames.length; j++) {
+          if ((result[i].title === freeGames[j].title) && (result[i].site === freeGames[j].site)) {
+            gameWasRemoved = false;
+          }    
+        }
+        if (gameWasRemoved) {
+          await FreeGame.findOneAndDelete({ "title": result[i].title, "site": result[i].site })
+            .then(result => {
+              log.push(`Removido o jogo ${result.title} (${result.site}).`)
+              // fs.appendFileSync('./log.txt', `Removido o jogo ${result.title} (${result.site}).\n`)
+              // console.log(`Removido o jogo ${result.title} (${result.site}).`)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    });
+
+  log.push(`Fim da atualização da Gog.`)
+  //fs.appendFileSync('./log.txt', `Fim da atualização\n\n`)
+  console.log(log)
+  await new UpdateLog({log}).save()
 }
+
 
 module.exports = { sendGogGames };
